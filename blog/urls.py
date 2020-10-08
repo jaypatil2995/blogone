3e3a1e645d1fbcaaf1996920fd6a7eeefb0f8f81
jaypatil2995 <jaypatil2995@gmail.com>
2020-10-08 04:32:30 -0800

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+app_name = 'blog'
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
+    path('<int:post_id>/share/',
+         views.post_share, name='post_share'),
+]
---
+++ blog/views.py
@@ -0,0 +1,97 @@
+from django.core.mail import send_mail
+from django.shortcuts import render,get_object_or_404,HttpResponse
+from .models import Post,Comment
+
+from django.core.paginator import Paginator, EmptyPage,\
+                                PageNotAnInteger
+
+from django.views.generic import ListView
+from .forms import EmailPostForm,CommentForm
+
+'''Create your views here.
+from django.contrib.auth.models import User
+from .models import	Post
+user = User.objects.get(username='admin')
+post = Post(title='Another	post',slug ='another-post',body ='Post	body.',author =user)
+post.save()
+
+def post_list(request):
+    #posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    #return render(request,'post/list.html',{'posts': posts})
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        #	If	page	is	not	an	integer	deliver	the	first	page
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'post/list.html',
+                  {'page': page,
+                   'posts': posts})'''
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'post/list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                                    status='published',
+                                    publish__year=year,
+                                    publish__month=month,
+                                    publish__day=day)
+    #	List	of	active	comments	for	this	post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        #	A	comment	was	posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            #	Create	Comment	object	but	don't	save	to	database	yet
+            new_comment = comment_form.save(commit=False)
+            #	Assign	the	current	post	to	the	comment
+            new_comment.post = post
+            #	Save	the	comment	to	the	database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request,'post/detail.html',{'post': post,	'comments':	comments,
+																			'new_comment':	new_comment,
+																			'comment_form':	comment_form})
+
+
+def	post_share(request,	post_id):
+    #	Retrieve	post	by	id
+    post = get_object_or_404(Post,	id=post_id,	status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                                        post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "  ' \
+                      '{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: ' \
+                      '{}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'jaypatil2995@gmail.com',
+                      [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request,'post/share.html',{'post': post,'form': form,	'sent':	sent})
+
+
+
+
+
+
+
